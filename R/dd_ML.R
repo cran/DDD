8cde305e74a7ce16a22842e6b8cdbcd94424b615
Rampal S. Etienne <r.s.etienne@rug.nl>
2012-01-31 16:44:50 +0000

    version 0.2

---
+++ R/dd_ML.R
@@ -0,0 +1,43 @@
+dd_ML = function(brts,initparsopt=c(0.2,0.1,2*length(brts)),idparsopt = 1:3,idparsfix = (1:3)[-idparsopt],parsfix = c(0.2,0.1,2*length(brts))[-idparsopt],res=10*(1+length(brts)),ddmodel=1,missnumspec=0)
+{
+# brts = branching times (positive, from present to past)
+# - max(brts) = crown age
+# - min(brts) = most recent branching time
+# - initpars[1] = la = (initial) speciation rate
+# - initpars[2] = mu = extinction rate
+# - initpars[3] = K = carrying capacity
+# - res = resolution of the method; res should be larger than the total number of species
+# - ddmodel = diversity-dependent model,mode of diversity-dependence
+#  . ddmodel == 1 : linear dependence in speciation rate
+#  . ddmodel == 2 : exponential dependence in speciation rate
+#  . ddmodel == 3 : linear dependence in extinction rate
+#  . ddmodel == 4 : exponential dependence in extinction rate
+# missnumspec = number of missing species    
+
+options(warn=-1)
+idpars = sort(c(idparsopt,idparsfix))
+if(sum(idpars == (1:3)) != 3) {cat("The parameters to be optimized and fixed are incoherent.") } else {
+
+namepars = c("la","mu","K")
+if(length(namepars[idparsopt]) == 0) { optstr = "nothing" } else { optstr = namepars[idparsopt] }
+cat("You are optimizing",optstr,"\n")
+if(length(namepars[idparsfix]) == 0) { fixstr = "nothing" } else { fixstr = namepars[idparsfix] }
+cat("You are fixing",fixstr,"\n")
+trparsopt = initparsopt/(1 + initparsopt)
+trparsfix = parsfix/(1 + parsfix)
+trparsfix[parsfix == Inf] = 1
+cat(noquote("Optimizing the likelihood - this may take a while."),"\n")
+#cat(noquote("Press enter to see some progress."),"\n")
+out = optimx(trparsopt,dd_loglik_choosepar,hess=NULL,method = "Nelder-Mead",control = list(maximize = TRUE,abstol = 1E-6,reltol = 1E-6,trace = 0,starttests = FALSE,kkt = FALSE),trparsfix = trparsfix,idparsopt = idparsopt,idparsfix = idparsfix,brts = brts,pars2 = c(res,ddmodel),missnumspec = missnumspec)
+MLtrpars = unlist(out$par)
+MLpars = MLtrpars/(1-MLtrpars)
+out$par = list(MLpars)
+MLpars1 = rep(0,3)
+MLpars1[idparsopt] = MLpars
+if(length(idparsfix) != 0) { MLpars1[idparsfix] = parsfix }
+s1 = sprintf('Maximum likelihood parameter estimates: %f %f %f',MLpars[1],MLpars[2],MLpars[3])
+s2 = sprintf('Maxmimum loglikelihood: %f',out$fvalues)
+cat("\n",s1,"\n",s2,"\n")
+return(out)
+}
+}
